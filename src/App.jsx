088840d5e068f48d93f5f9dc5c94b0647dd98ab8
bishal lamichhane <bishallamichhane@bishals-MacBook-Pro.local>
2024-12-25 19:13:24 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import NewsBlock from "./NewsBlock";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import NewsArticle from "./NewsArticle";
+
+export default function App() {
+  const [Products, setProducts] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch(`https://fakestoreapi.com/products/`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const NewsList = Products.map(function (item) {
+    return (
+      <NewsArticle
+        productImage={item.image}
+        productDesc={item.description}
+        productPrice={item.price}
+        productName={item.title}
+      />
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+           <h1 class="text-center">Taazaa Store</h1>
+        </div>
+      </div>
+      <div className="row">{NewsList}</div>
+    </div>
+  );
+}
